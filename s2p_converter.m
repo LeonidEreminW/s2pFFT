9eace9eb92e45b1f0dec0b2a2db9bfccd302c9f2
L.Er <leonidereminw@gmail.com>
2020-06-05 01:36:39 +0700

    prototype of converter

--- s2p_converter.m
+++ s2p_converter.m
@@ -5,18 +5,16 @@ fname = 'testdata.s2p';
 [header, data] = read_s2p (fname);
 disp(header)
 T = 1/(data(2,1) - data(1,1))
-N = size(Data, 1)
+N = size(data, 1)
 n = 0:(N-1);
 tn = T/(N-1)*n;
 fk = n/T;
 if header.format == 'RI'
 Xkk = data(:, 2) + j*data(:,3);
 xnn = ifft(Xkk);
-subplot(3, 1, 1)
-plot(tn,xn)
-fk = n/T;
-subplot(3, 1, 2)
-plot(fk,real(Xkk))
-subplot(3, 1, 3)
+subplot(2, 1, 1)
 plot(tn, xnn)
+fk = n/T;
+subplot(2, 1, 2)
+plot(fk,real(Xkk))
 endif

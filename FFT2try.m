57d7055e18b411b9b8f6cf95fcf31a27c24afdd3
L.Er <leonidereminw@gmail.com>
2020-06-04 22:08:20 +0700

    add plots to show FFT

--- FFT2try.m
+++ FFT2try.m
@@ -1,23 +1,31 @@
-N = 11
+N = 1000
 Xk = zeros(N,1);
-m = 2;
-Xk(m+1) = 2;
+Xk(2+1) = 2;
+Xk(4+1) = 2*3;
 ##Xk(N-m+1) = 1;
 xn = zeros(N,1);
 
-for n = 0:N-1
+n = (0:N-1)';
   for k = 0:N-1
     c = cos(2*pi*n*k/N);
- # s = sin(2*pi*n*k/N) ;
+##    c = 0;
+##    s = sin(2*pi*n*k/N) ;
     s = 0;
-    xn(n+1) += Xk(k+1)*(c + j*s);
+    xn += Xk(k+1)*(c + s);
   endfor
-endfor
+  
 xn /= N;
-disp('xn')
-disp(xn)
-disp('fft')
+##disp('xn')
+##disp(xn)
+##disp('fft')
 Xkk = fft(xn);
-disp(Xkk)
-plot(xn)
-print_column(Xk, 'Xk')
+##disp(Xkk)
+##print_column(Xk, 'Xk')
+
+T = 2*pi*4;
+tn = T/(N-1)*n;
+subplot(2, 1, 1)
+plot(tn,xn)
+fk = n/T
+subplot(2, 1, 2)
+plot(fk,real(Xkk))
